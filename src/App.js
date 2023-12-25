d3f0c764bb7b3978e63026b72fedaeee0f91b6ae
Kyaw San Maung <kyawsanmaung262019@gmail.com>
2023-12-25 18:39:52 +0630

    Adding Home component and Cart Component in the App.js.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { BrowserRouter, Route } from "react-router-dom";
 import "./App.css";
 import Header from "./components/Header";
+import Home from "./components/Home";
+import Cart from "./components/Cart";
 
 function App() {
   return (
