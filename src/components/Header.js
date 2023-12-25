1197f48ff5c4f1b440c13ea05e20bc30bc3d15ab
Kyaw San Maung <kyawsanmaung262019@gmail.com>
2023-12-25 14:30:16 +0630

    Installing Fontawsome for react and creating navigation bar for our project

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,39 @@
-import React from 'react'
-import { Container, Navbar } from 'react-bootstrap'
+import React from "react";
+import {
+  Badge,
+  Container,
+  Dropdown,
+  FormControl,
+  Nav,
+  Navbar,
+} from "react-bootstrap";
 
 export default function () {
   return (
-    <Navbar>
-          <Container>
-              <Navbar.Brand>
-                  <a>Signora Austetic</a>
-              </Navbar.Brand>
-        </Container>
+    <Navbar bg="dark" variant="dark" style={{ height: 80 }}>
+      <Container>
+        <Navbar.Brand>
+          <a href="/">Signora Austetic</a>
+        </Navbar.Brand>
+        <Navbar.Text className="search">
+          <FormControl
+            style={{ width: 500 }}
+            placeholder="search product.."
+            className="m-auto"
+          />
+        </Navbar.Text>
+        <Nav>
+          <Dropdown>
+            <Dropdown.Toggle variant="success" id="dropdown-basic">
+              <Badge>{10}</Badge>
+            </Dropdown.Toggle>
+
+            <Dropdown.Menu style={{ minWidth: 370 }}>
+              <span style={{ padding: 10 }}>Cart is Empty</span>
+            </Dropdown.Menu>
+          </Dropdown>
+        </Nav>
+      </Container>
     </Navbar>
-  )
+  );
 }
