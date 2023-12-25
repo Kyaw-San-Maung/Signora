188cfefe43279f9f6dfe5fda4bee4d7433f5c991
Kyaw San Maung <kyawsanmaung262019@gmail.com>
2023-12-25 14:10:46 +0630

    Setting up and creating navigation bar for my project.

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Container, Navbar } from 'react-bootstrap'
+
+export default function () {
+  return (
+    <Navbar>
+          <Container>
+              <Navbar.Brand>
+                  <a>Signora Austetic</a>
+              </Navbar.Brand>
+        </Container>
+    </Navbar>
+  )
+}
