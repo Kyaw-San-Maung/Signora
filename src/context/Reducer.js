d77b3b38d2250f371333002e575a4455a6272abe
Kyaw San Maung <kyawsanmaung262019@gmail.com>
2023-12-28 22:41:06 +0630

    Updating my new Reducer to do for filter part.

--- src/context/Reducer.js
+++ src/context/Reducer.js
@@ -22,7 +22,25 @@ export default function cartReducer(state, action) {
 }
 
 export const productReducer = (state, action) => {
-  
-}
-
-
+  switch (action.type) {
+    case "SORT_BY_PRICE":
+      return { ...state, sort: action.payload };
+    case "FILTER_BY_STOCK":
+      return { ...state, byStock: !state.byStock };
+    case "FILTER_BY_DELIVERY":
+      return { ...state, byFasDelivery: !state.byFasDelivery };
+    case "FILTER_BY_RATING":
+      return { ...state, byRating: action.payload };
+    case "FILTER_BY_SEARCH":
+      return { ...state, searchQuery: action.payload };
+    case "CLEAR_FILTERS":
+      return {
+        byStock: false,
+        byFasDelivery: false,
+        byRating: 0,
+        searchQuery: "",
+      };
+    default:
+      break;
+  }
+};
